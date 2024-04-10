a85f282603a74f049b1b9587e5041a27527e560c
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-04-10 16:44:30 -0300

    ClasseProduto(Q8).java

---
+++ ClasseProduto(Q8).java
@@ -0,0 +1,32 @@
+public class Produto {
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto(String n, double p, int q) {
+		this.nome = n;
+		this.preco = p;
+		this.quantidade = q;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double CalcularTotal() {
+		return (this.preco * this.quantidade);
+	}
+}

5f37bf8e824c19bd56d565d22e4617f72015ddc6
Jaldson Arthur <126914358+JaldsonArthur17@users.noreply.github.com>
2024-04-10 16:39:17 -0300

    Create ClasseCalculadora(Q3).java

---
+++ ClasseCalculadora(Q3).java
@@ -0,0 +1,15 @@
+public class Calculadora {
+	
+    public int soma(int a, int b) {
+    	return a + b;	
+    }
+    public int subtrair(int a, int b) {
+    	return a - b;
+    }
+	public int multiplicação(int a, int b) {
+		return a * b;
+	}
+	public int dividir(int a, int b) {
+		return a / b;
+	}
+}
